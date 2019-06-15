0b0bab7c61b2507ffe5e4afc307ab27a98c16b0f
mrh <mamadreza.hajeb@gmail.com>
2019-06-15 17:45:55 +0430

    Init commit

---
+++ src/store/configureStore.js
@@ -0,0 +1,15 @@
+import {combineReducers, createStore, applyMiddleware, compose} from "redux";
+import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+    return createStore(
+        combineReducers({
+            auth: authReducer
+        }),
+        composeEnhancers(applyMiddleware(thunk))
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+};
